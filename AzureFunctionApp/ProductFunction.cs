d8434f773fe4ce2791304e41f0d069b13816be53
Metin Çelebi <metincelebi5534@gmail.com>
2022-10-27 23:26:05 +0300

    Add project files.

---
+++ AzureFunctionApp/ProductFunction.cs
@@ -0,0 +1,81 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using AzureFunctionApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace AzureFunctionApp
+{
+    public class ProductFunction
+    {
+        private const string Route = "Products";
+        private readonly AppDbContext _dbContext;
+        public ProductFunction(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [FunctionName("GetProducts")]
+        public async Task<IActionResult> GetProducts(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = Route)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("tüm ürünleri getir");
+            var products = await _dbContext.Products.ToListAsync();
+
+            return new OkObjectResult(products);
+        }
+
+        [FunctionName("SaveProducts")]
+        public async Task<IActionResult> SaveProducts(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = Route)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("ürün ekle");
+
+            string body = await new StreamReader(req.Body).ReadToEndAsync();
+            var newProduct = JsonConvert.DeserializeObject<Product>(body);
+
+            _dbContext.Products.Add(newProduct);
+            await _dbContext.SaveChangesAsync();
+
+            return new OkObjectResult(newProduct);
+        }
+
+        [FunctionName("UpdateProducts")]
+        public async Task<IActionResult> UpdateProducts(
+            [HttpTrigger(AuthorizationLevel.Function, "put", Route = Route)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("ürün güncelle");
+
+            string body = await new StreamReader(req.Body).ReadToEndAsync();
+            var newProduct = JsonConvert.DeserializeObject<Product>(body);
+
+            _dbContext.Products.Update(newProduct);
+            await _dbContext.SaveChangesAsync();
+
+            return new NoContentResult();
+        }
+
+        [FunctionName("DeleteProducts")]
+        public async Task<IActionResult> DeleteProducts(
+            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = Route+"/{id}")] HttpRequest req,
+            ILogger log, int id)
+        {
+            log.LogInformation("ürün sil");
+
+            var product = await _dbContext.Products.FindAsync(id);
+
+            _dbContext.Products.Remove(product);
+            await _dbContext.SaveChangesAsync();
+
+            return new NoContentResult();
+        }
+    }
+}
---
+++ AzureFunctionApp/Startup.cs
@@ -0,0 +1,21 @@
+﻿using AzureFunctionApp.Models;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(AzureFunctionApp.Startup))]
+namespace AzureFunctionApp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var constr = Environment.GetEnvironmentVariable("SqlConnectionString");
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(constr);
+            });
+        }
+    }
+}
