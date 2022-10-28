0eb4c06610dbe523419cdd80bd9168c2e504c3d2
Metin Çelebi <metincelebi5534@gmail.com>
2022-10-28 15:26:25 +0300

    Output Blob binding done.

--- TimerTriggerFunctionApp/TimerTriggerFunction.cs
+++ TimerTriggerFunctionApp/TimerTriggerFunction.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Text;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -7,8 +9,13 @@ namespace TimerTriggerFunctionApp
     public class TimerTriggerFunction
     {
         [FunctionName("Function1")]
-        public void Run([TimerTrigger("* * * * * *")]TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("*/3 * * * * *")] TimerInfo myTimer, ILogger log, [Blob("logs/data.txt", FileAccess.Write, Connection = "LocalAzureStorage")] Stream blobStream)
         {
+            var ifade = Encoding.UTF8.GetBytes($"Logs: {DateTime.Now}");
+            blobStream.Write(ifade, 0, ifade.Length);
+            blobStream.Close();
+            blobStream.Dispose();
+
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
     }
