25e382dd8675e7d5ac46976be6daf6b48edd69a4
Metin Çelebi <metincelebi5534@gmail.com>
2022-10-28 20:02:29 +0300

    Output Blob binding Rezize project done.

--- BlobTriggerFunctionApp/BlobTriggerFunction.cs
+++ BlobTriggerFunctionApp/BlobTriggerFunction.cs
@@ -1,15 +1,24 @@
-using System.IO;
+﻿using System.IO;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace BlobTriggerFunctionApp
 {
     public class BlobTriggerFunction
     {
-        [FunctionName("Function1")]
-        public void Run([BlobTrigger("udemy-pictures/{name}", Connection = "LocalAzureStorage")]Stream myBlob, string name, ILogger log)
+        [FunctionName("ResizeFunction")]
+        public void ResizeFunction([BlobTrigger("udemy-pictures/{name}", Connection = "LocalAzureStorage")] Stream myBlob, string name, ILogger log, [Blob("udemy-pictures-resize/{name}", FileAccess.Write, Connection = "LocalAzureStorage")] Stream outputBlob)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            var format = Image.DetectFormat(myBlob);
+            var resizeImage = Image.Load(myBlob);
+
+            resizeImage.Mutate(x => x.Resize(100, 100));
+            resizeImage.Save(outputBlob, format);
+
+            log.LogInformation("Resim resize yapıldı.");
         }
     }
 }
