37c9beee22d212d1ebcf2cedc2dcf14ff0d31c9c
Metin Çelebi <metincelebi5534@gmail.com>
2022-10-28 13:06:48 +0300

    class name change done.

--- TimerTriggerFunctionApp/TimerTriggerFunction.cs
+++ TimerTriggerFunctionApp/TimerTriggerFunction.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Logging;
 
 namespace TimerTriggerFunctionApp
 {
-    public class Function1
+    public class TimerTriggerFunction
     {
         [FunctionName("Function1")]
         public void Run([TimerTrigger("* * * * * *")]TimerInfo myTimer, ILogger log)
